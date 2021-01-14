5880cd2c8ecd06a560ebcc5f94e124a2eb55f603
Pavel Kostyukov <nik-kostukov@yandex.ru>
2021-01-14 15:42:00 +0300

    Убраны лишние файлы. Удален бандл диплоя

--- config/bundles.php
+++ config/bundles.php
@@ -5,5 +5,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    EasyCorp\Bundle\EasyDeployBundle\EasyDeployBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
